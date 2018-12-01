52af4a69e6436d1e807514f5330cf11958b1e8fd
Jim Moua <jimmoua97@live.com>
2018-12-01 15:19:19 -0600

    Got moving working. Need to implement a method that checks for tile collision.

--- MouseType.cpp
+++ MouseType.cpp
@@ -1,5 +1,65 @@
 #include "MouseType.h"
 
-mouseStatusEnum mouseType::getMouseState() const {
-  return mouseState;
+// Externs
+sf::Clock mouseType::_mouseMoveWaitTime;
+
+/* ----------------------------------------------------------------------------
+ * Function:
+ *   mouseType(std::pair<int, int>)
+ * Description:
+ *   Default ctor
+ * --------------------------------------------------------------------------*/
+mouseType::mouseType(std::pair<int, int> pos) {
+  _mouseTexture.loadFromFile(assetsObj.getGraphicsName(graphics::TILE_MOUSE));
+  _mouseSprite.setTexture(_mouseTexture);
+  _mouseSprite.setPosition(pos.first*64, pos.second*64);
+}
+
+/* ----------------------------------------------------------------------------
+ * Function:
+ *   getMouseState()
+ *
+ * Description:
+ *   Gets the current state of the mouse.
+ * --------------------------------------------------------------------------*/
+mouseStatusEnum mouseType::_getMouseState() const {
+  return _mouseState;
+}
+
+
+/* ----------------------------------------------------------------------------
+ * Function:
+ *   resetWaitTime()
+ * Description:
+ *   If the clock can be reseted, reset it and move the mouse a random
+ *   location.
+ * --------------------------------------------------------------------------*/
+void mouseType::_move() {
+  auto t = _mouseMoveWaitTime.getElapsedTime().asSeconds();
+  if(t > sf::seconds(0.25f).asSeconds()) {
+    // Let the randomly generated number  below represent the follow:
+    //   0 = Up
+    //   1 = Down
+    //   2 = Left
+    //   3 = Right
+    srand(time(NULL));
+    unsigned short int move = rand()%4;
+    switch(move) {
+      case 0:
+        _mouseSprite.move(0, 64);
+        break;
+      case 1:
+        _mouseSprite.move(0, -64);
+        break;
+      case 2:
+        _mouseSprite.move(64, 0);
+        break;
+      case 3:
+        _mouseSprite.move(-64, 0);
+        break;
+    }
+    _mouseMoveWaitTime.restart();
+  }
+  // Draw the mouse
+  winObj._getRefSFMLWindow()->draw(_mouseSprite);
 }
--- MouseType.h
+++ MouseType.h
@@ -1,6 +1,10 @@
 #ifndef MOUSE_TYPE_H
 #define MOUSE_TYPE_H
+#include <time.h>
+#include <cstdlib>
 #include <SFML/Graphics.hpp>
+#include "Window.h"
+#include "asset.h"
 
 /* This is the class that will be dealing with the entity of the mouse. This
  * means that in here, we are handling what the mouse does, its number of
@@ -18,11 +22,18 @@ enum class mouseStatusEnum {
 // Mouse class
 class mouseType {
   public:
-    mouseStatusEnum getMouseState() const;
+    mouseType(std::pair<int, int>);
+    mouseStatusEnum _getMouseState() const;
+    void _move();
   private:
-    const unsigned short int maxMouseMoves = 50;
-    mouseStatusEnum mouseState;
-    std::pair<int, int> currentMouseLoc;
+    const unsigned short int _maxMouseMoves = 50;
+    mouseStatusEnum _mouseState;
+    std::pair<int, int> _currentMouseLoc;
+    static sf::Clock _mouseMoveWaitTime;
+
+    // Mouse images
+    sf::Texture _mouseTexture;
+    sf::Sprite _mouseSprite;
 };
 
 #endif
