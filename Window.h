db0849565d192ac06c030d26f3ed0a950b790c7e
Jim Moua <jimmoua97@live.com>
2018-12-01 01:03:50 -0600

    Fixed extern issue. I had them declared in the namespace scope, but did not define them. Defined them in main.

--- Window.h
+++ Window.h
@@ -3,39 +3,56 @@
 #include <SFML/Graphics.hpp>
 #include <cstddef>
 #include <string>
+#include <iostream>
 
-class sfmlWindowClass {
-  public:
-    sfmlWindowClass();
-    sf::Vector2u _getWindowResoSize() const;
-    sf::RenderWindow& _getRefSFMLWindow();
-    sf::Event& _getEvent();
-  private:
-    sf::RenderWindow _rWindow;
-    sf::Vector2u _rWindowSizeResolution = sf::Vector2u(720, 720);
-    sf::Event _windowEvent; // Use this to poll windows in different states
-};
+namespace dm {
+  class sfmlWindowClass {
+    public:
+      sfmlWindowClass();
+      sf::Vector2u _getWindowResoSize() const;
+      sf::RenderWindow& _getRefSFMLWindow();
+      sf::Event& _getEvent();
+    private:
+      static sf::RenderWindow _rWindow;
+      static sf::Vector2u _rWindowSizeResolution;
+      sf::Event _windowEvent; // Use this to poll windows in different states
+  };
 
-sf::Vector2u sfmlWindowClass::_getWindowResoSize() const {
-  return _rWindowSizeResolution;
-}
+  // Static members
+  sf::Vector2u sfmlWindowClass::_rWindowSizeResolution;
+  sf::RenderWindow sfmlWindowClass::_rWindow;
 
-sf::RenderWindow& sfmlWindowClass::_getRefSFMLWindow() {
-  return _rWindow;
-}
+  sf::Vector2u sfmlWindowClass::_getWindowResoSize() const {
+    return _rWindowSizeResolution;
+  }
 
-sfmlWindowClass::sfmlWindowClass() {
-  _rWindow.create(
-      // Specify a resolution size
-      sf::VideoMode(_rWindowSizeResolution.x, _rWindowSizeResolution.y),
-      // Window  Title
-      "Dead Mouse SFML",
-      // Window options
-      sf::Style::Close);
-}
+  sf::RenderWindow& sfmlWindowClass::_getRefSFMLWindow() {
+    return _rWindow;
+  }
 
-sf::Event& sfmlWindowClass::_getEvent() {
-  return _windowEvent;
+  sfmlWindowClass::sfmlWindowClass() {
+    // Get a specific resolution...
+    unsigned short int x, y;
+    std::cout << "Enter in width and height: ";
+    std::cin >> x >> y;
+    // Each width and height is multiplied by 64 to represent resolution.
+    _rWindowSizeResolution.x = x*64;
+    _rWindowSizeResolution.y = y*64;
+
+    // Create the SMFL window
+    _rWindow.create(
+        // Specify a resolution size
+        sf::VideoMode(_rWindowSizeResolution.x, _rWindowSizeResolution.y),
+        // Window  Title
+        "Dead Mouse SFML",
+        // Window options
+        sf::Style::Close);
+  }
+
+  sf::Event& sfmlWindowClass::_getEvent() {
+    return _windowEvent;
+  }
+  extern sfmlWindowClass winObj;
 }
 
 #endif
