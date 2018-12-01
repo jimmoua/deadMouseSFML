fab5ba4ce9d11cdd0065be7ad8c5dd125825836c
Jim Moua <jimmoua97@live.com>
2018-12-01 12:24:46 -0600

    Small log changes

--- log.cpp
+++ log.cpp
@@ -1,22 +1,20 @@
 #include "log.h"
 
-namespace logger {
-  // Define externs
-  log logger;
+// Define externs
+log logger;
 
-  log::log() {
-    // New log every run
-    if(std::filesystem::exists("./logs/errors.log"))
-      std::filesystem::remove("./logs/errors.log");
-  }
+log::log() {
+  // New log every run
+  if(std::filesystem::exists("./logs/errors.log"))
+    std::filesystem::remove("./logs/errors.log");
+}
 
-  void log::err(const std::string& msg) {
-    out.open(errorFile, std::ios::app);
-    // Get the system time
-    auto time
-      = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    // Log time and error message
-    out << std::ctime(&time) << ": " << msg << std::endl;
-    out.close();
-  }
+void log::err(const std::string& msg) {
+  out.open(errorFile, std::ios::app);
+  // Get the system time
+  auto time
+    = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  // Log time and error message
+  out << std::ctime(&time) << ": " << msg << std::endl;
+  out.close();
 }
--- log.h
+++ log.h
@@ -3,16 +3,14 @@
 #include <string>
 #include <filesystem>
 
-namespace logger {
-  class log {
-    public:
-      log();
-      void err(const std::string&);
-    private:
-      std::ofstream out;
-      const std::string errorFile = "errors.log";
-  };
+class log {
+  public:
+    log();
+    void err(const std::string&);
+  private:
+    std::ofstream out;
+    const std::string errorFile = "errors.log";
+};
 
-  // Define extern
-  extern log logger;
-}
+// Define extern
+extern log logger;
