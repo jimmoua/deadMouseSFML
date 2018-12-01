8b5b76433cbd7b71a409ed6954a8792b114b9b82
Jim Moua <jimmoua97@live.com>
2018-11-30 21:17:58 -0600

    Added basic idea of looping comments to deadMouse.cpp

--- deadMouse.cpp
+++ deadMouse.cpp
@@ -1,5 +1,6 @@
 #include "IslandType.h"
 #include "Window.h"
+#include "asset.h"
 
 int main() {
   // TODO: Get a working displaying window that can loop
@@ -9,9 +10,18 @@ int main() {
 
   while(objWin._getRefSFMLWindow().isOpen()) {
     while(objWin._getRefSFMLWindow().pollEvent(objWin._getEvent())) {
+      // Poll the window
       if(objWin._getEvent().type == sf::Event::Closed)
         objWin._getRefSFMLWindow().close();
     }
+
+    // Paint window black
+    objWin._getRefSFMLWindow().clear(sf::Color::Black);
+
+    // What to draw?
+
+    // Display the window.
+    objWin._getRefSFMLWindow().display();
   }
   return EXIT_SUCCESS;
 }
