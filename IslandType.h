16c03d4903805a68c4581ba411f14d657f938e35
Jim Moua <jimmoua97@live.com>
2018-12-01 01:37:03 -0600

    Added classes to namespace dm.

--- IslandType.h
+++ IslandType.h
@@ -1,6 +1,13 @@
 #ifndef ISLAND_TYPE_H
 #define ISLAND_TYPE_H
+#include <SFML/Graphics.hpp>
+#include "asset.h"
 
-
+namespace dm {
+  class islandType {
+    public:
+    private:
+  };
+}
 
 #endif
--- asset.h
+++ asset.h
@@ -3,38 +3,45 @@
 #include <SFML/Graphics.hpp>
 #include <map>
 
-enum class graphics {
-  TILE_BRIDGE,
-  TILE_WATER,
-  TILE_LAND,
-  TILE_MOUSE,
+namespace dm {
+  enum class graphics {
+    TILE_BRIDGE,
+    TILE_WATER,
+    TILE_LAND,
+    TILE_MOUSE,
 
-  WALLPAPER_TITLE
-};
+    WALLPAPER_TITLE
+  };
 
-class assets {
-  public:
-    assets();
-  private:
-    static sf::Font gameFont;
-    
-    // Container to hold graphics. Second argument is file loc
-    std::map<graphics, std::string> _assetGraphpics;
-};
+  class assets {
+    public:
+      assets();
+      std::string& getGraphicsName(graphics& key);
+    private:
+      static sf::Font gameFont;
+      
+      // Container to hold graphics. Second argument is file loc
+      std::map<graphics, std::string> _assetGraphpics;
+  };
 
-// Default ctor
-assets::assets() {
-  // Load graphics for tiles and characters.
-  _assetGraphpics[graphics::TILE_BRIDGE] = "./assets/tiles/bridge.png";
-  _assetGraphpics[graphics::TILE_WATER] = "./assets/tiles/water.png";
-  _assetGraphpics[graphics::TILE_LAND] = "./assets/tiles/grass.png";
-  _assetGraphpics[graphics::TILE_MOUSE] = "./assets/tiles/red_slime.png";
+  // Default ctor
+  assets::assets() {
+    // Load graphics for tiles and characters.
+    _assetGraphpics[graphics::TILE_BRIDGE] = "./assets/tiles/bridge.png";
+    _assetGraphpics[graphics::TILE_WATER] = "./assets/tiles/water.png";
+    _assetGraphpics[graphics::TILE_LAND] = "./assets/tiles/grass.png";
+    _assetGraphpics[graphics::TILE_MOUSE] = "./assets/tiles/red_slime.png";
+  }
+
+  std::string& assets::getGraphicsName(graphics& key) {
+    return _assetGraphpics[key];
+  }
+
+  // Static declarations
+  sf::Font assets::gameFont;
+
+  // extern this so accessible by all.
+  extern assets assetsObj;
 }
-
-// Static declarations
-sf::Font assets::gameFont;
-
-// extern this so accessible by all.
-extern assets assetsObj;
 
 #endif
